f17655c6b2a2eb80a8639648f550d5528fecc5dc
Nathaniel Hobbs <hobbs.nathaniel@gmail.com>
2026-09-17 12:01:39 -0400

    inititial commit

---
+++ number_stuff.py
@@ -0,0 +1,25 @@
+3
+3+1
+
+print(4+5)
+
+print(type(3)) # see that 3 is an object of integer class
+print(type(type))
+print(type(print))
+
+x=3
+print(type(x))
+print(type(3.0))
+
+print(3+1.0) # float wins
+print(3+3) # addition
+print(3-3) # subtraction
+print(2*3) # multipliation
+print(3/3) # division *always* returns float
+
+print(20//7) # whole division (how many times 7 evenely divides 20)
+print(20%7) # remainder after division. read "modulo" or "mod"
+print(2**8) # exponentiation
+
+print(int(3.8)) # cast float as int, takes the floor
+print(float(1)) # cast integer as float
